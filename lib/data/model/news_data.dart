caa475f38e9da16661e45197c1c3051c7635dea1
nputrautama <nyoman.putra@ruangguru.com>
2022-02-20 18:21:08 +0800

    feat: add news data model

---
+++ lib/data/model/news_data.dart
@@ -0,0 +1,65 @@
+class NewsData {
+  NewsData({
+    this.status,
+    this.totalResults,
+    this.articles,
+  });
+
+  final String? status;
+  final int? totalResults;
+  final List<Article>? articles;
+
+  factory NewsData.fromJson(Map<String, dynamic> json) => NewsData(
+    status: json["status"],
+    totalResults: json["totalResults"],
+    articles: List<Article>.from(json["articles"].map((x) => Article.fromJson(x))),
+  );
+}
+
+class Article {
+  Article({
+    this.source,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  });
+
+  final Source? source;
+  final String? author;
+  final String? title;
+  final String? description;
+  final  String? url;
+  final String? urlToImage;
+  final DateTime? publishedAt;
+  final String? content;
+
+  factory Article.fromJson(Map<String, dynamic> json) => Article(
+    source: Source.fromJson(json["source"]),
+    author: json["author"],
+    title: json["title"],
+    description: json["description"],
+    url: json["url"],
+    urlToImage: json["urlToImage"],
+    publishedAt: DateTime.parse(json["publishedAt"]),
+    content: json["content"],
+  );
+}
+
+class Source {
+  Source({
+    this.id,
+    this.name,
+  });
+
+  final String? id;
+  final String? name;
+
+  factory Source.fromJson(Map<String, dynamic> json) => Source(
+    id: json["id"],
+    name: json["name"],
+  );
+}
