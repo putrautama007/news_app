4c2ae08bb72c331f2c3c040ab6078a41ea584091
nputrautama <nyoman.putra@ruangguru.com>
2022-02-27 23:12:57 +0800

    feat: add news entity

---
+++ lib/domain/entity/news_entity.dart
@@ -0,0 +1,51 @@
+import 'package:equatable/equatable.dart';
+
+class NewsEntity extends Equatable {
+  const NewsEntity({
+    this.source,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  });
+
+  final NewsSource? source;
+  final String? author;
+  final String? title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final DateTime? publishedAt;
+  final String? content;
+
+  @override
+  List<Object?> get props => [
+        source,
+        author,
+        title,
+        description,
+        url,
+        urlToImage,
+        publishedAt,
+        content,
+      ];
+}
+
+class NewsSource extends Equatable {
+  const NewsSource({
+    this.id,
+    this.name,
+  });
+
+  final String? id;
+  final String? name;
+
+  @override
+  List<Object?> get props => [
+        id,
+        name,
+      ];
+}
