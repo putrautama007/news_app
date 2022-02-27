6df700e0fe07775a7d1e123d1a92a8d7d569af4f
nputrautama <nyoman.putra@ruangguru.com>
2022-02-27 22:46:46 +0800

    refactor: move network to utils directory

--- lib/di/locator.dart
+++ lib/di/locator.dart
@@ -1,8 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:news_app/data/datasource/remote/news_datasource.dart';
-import 'package:news_app/network/api_helper.dart';
-import 'package:news_app/network/api_helper_impl.dart';
+import 'package:news_app/utils/network/api_helper.dart';
+import 'package:news_app/utils/network/api_helper_impl.dart';
 
 final locator = GetIt.instance;
 
