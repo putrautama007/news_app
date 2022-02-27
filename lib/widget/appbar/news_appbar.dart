f7d76faf4f283c7101e8292e3f6171418981bc56
nputrautama <nyoman.putra@ruangguru.com>
2022-02-27 22:30:02 +0800

    feat: add on top on header appbar

--- lib/widget/appbar/news_appbar.dart
+++ lib/widget/appbar/news_appbar.dart
@@ -2,11 +2,16 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:news_app/data/model/news_data.dart';
+import 'package:news_app/router/news_router.dart';
+import 'package:news_app/utils/navigation/navigation_helper.dart';
 
 class NewsAppBar extends StatelessWidget {
   final Article article;
+  final NewsRouter _newsRouter = NewsRouterImpl(
+    navigationHelper: NavigationHelperImpl(),
+  );
 
-  const NewsAppBar({Key? key, required this.article}) : super(key: key);
+  NewsAppBar({Key? key, required this.article}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,59 +23,63 @@ class NewsAppBar extends StatelessWidget {
           children: [
             article.urlToImage == null
                 ? SizedBox(
-                    child: const Center(
-                      child: Icon(Icons.error),
-                    ),
-                    width: 1.sw,
-                    height: 298.h,
-                  )
+              child: const Center(
+                child: Icon(Icons.error),
+              ),
+              width: 1.sw,
+              height: 298.h,
+            )
                 : Stack(
-                    children: [
-                      GestureDetector(
-                        // onTap: () => Navigator.pushNamed(
-                        //     context, DetailNewsPage.routeName,
-                        //     arguments: article),
-                        child: CachedNetworkImage(
-                          height: 200.h,
-                          width: 1.sw,
-                          imageUrl: article.urlToImage!,
-                          fit: BoxFit.cover,
-                          placeholder: (context, url) => SizedBox(
-                            child: const Center(
-                                child: CircularProgressIndicator()),
-                            height: 200.h,
-                            width: 1.sw,
-                          ),
-                          errorWidget: (context, url, error) => SizedBox(
-                            child: const Center(
-                              child: Icon(Icons.error),
-                            ),
-                            width: 1.sw,
-                            height: 200.h,
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Card(
-                          margin: const EdgeInsets.all(0),
-                          shape: const BeveledRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(10),
-                            ),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(10),
-                            child: Text(
-                              "New",
-                              style: Theme.of(context).textTheme.headline6,
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+              children: [
+                GestureDetector(
+                  onTap: () => _newsRouter.navigateToDetailNews(
+                    article: article,
                   ),
+                  child: CachedNetworkImage(
+                    height: 200.h,
+                    width: 1.sw,
+                    imageUrl: article.urlToImage!,
+                    fit: BoxFit.cover,
+                    placeholder: (context, url) => SizedBox(
+                      child: const Center(
+                          child: CircularProgressIndicator()),
+                      height: 200.h,
+                      width: 1.sw,
+                    ),
+                    errorWidget: (context, url, error) => SizedBox(
+                      child: const Center(
+                        child: Icon(Icons.error),
+                      ),
+                      width: 1.sw,
+                      height: 200.h,
+                    ),
+                  ),
+                ),
+                const Align(
+                  alignment: Alignment.topLeft,
+                  child: Card(
+                    color: Colors.blue,
+                    margin: EdgeInsets.all(0),
+                    shape: BeveledRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(10),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        "New",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
             Container(
               padding: const EdgeInsets.all(5),
               height: 90.h,
