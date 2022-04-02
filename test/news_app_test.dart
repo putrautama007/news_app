65ebb4b37b6076657a234747d1e3c5d604f86a91
nputrautama <nyoman.putra@ruangguru.com>
2022-04-02 11:06:13 +0800

    test: domain test case

--- test/news_app_test.dart
+++ test/news_app_test.dart
@@ -2,10 +2,14 @@ import '../feature/home/test/home_test.dart' as home_test;
 import '../feature/news_list/test/news_list_test.dart' as news_list_test;
 import '../feature/news_favorite/test/news_favorite_test.dart'
     as news_favorite_test;
+import '../domain/news/test/news_test.dart' as news_test;
 
 void main() {
-  /// Bloc
+  /// Feature
   home_test.main();
   news_list_test.main();
   news_favorite_test.main();
+
+  /// Domain
+  news_test.main();
 }
